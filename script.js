3eda52c68045affe1e6e0535ac57b11504554703
Francisco Cruz <fcocruz1107@gmail.com>
2023-01-10 20:27:16 -0300

    Aula 10 01 - segunda etapa

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const linkPerfil = document.getElementById("link-perfil");
 const navPerfil = document.getElementById("nav-perfil");
+const linkPerfilDados = document.getElementById("link-perfil-dados")
 
 linkPerfil.addEventListener("mouseover", ()=> {
   navPerfil.style.display = "block"
 })
 
-window.addEventListener("keyup", (e)=>{
+window.addEventListener("keyup",(e)=>{
   console.log(e.key);
   console.log(e.code);
-  if(e.code == "Digit1"){
-    console.log("Abrir navegação secundária")
+  if(navPerfil.style.display == "block"){
+    if(e.code == "Digit1"){
+      linkPerfilDados.click()
+    }
+  } else if(e.code == "Digit1"){
+    console.log("Abrir navegação secundaria")
     navPerfil.style.display = "block"
   }
 })
